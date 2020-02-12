61aeb7e40d555da5481b988820155ac4b2289d59
luisfff29 <luisfff29@gmail.com>
2020-02-12 06:12:30 -0600

    #all for debug purpose

--- nested.py
+++ nested.py
@@ -5,19 +5,19 @@ Module docstring: One line description of what your program does.
 """
 __author__ = "luisfff29"
 
-import sys
+# import sys
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    print(line)
 
 
-def main(args):
-    """Open the input file and call `is_nested()` for each line"""
-    # Results: print to console and also write to output file
-    pass
+is_nested('(*a++(*)')
+# def main(args):
+#     """Open the input file and call `is_nested()` for each line"""
+#     # Results: print to console and also write to output file
+#     pass
 
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+# if __name__ == '__main__':
+#     main(sys.argv[1:])
